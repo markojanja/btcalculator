9d153c14817e379e0314e984ae5b8cf2e739adbf
Marko Janjic <marko.m.janjic@gmail.com>
2025-03-31 01:21:11 +0200

    updated front

--- frontend/src/components/Converter.jsx
+++ frontend/src/components/Converter.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import CalculatorHeading from "./CalculatorHeading";
 
 const Converter = () => {
   const [jsonFile, setJsonFile] = useState(null);
@@ -7,6 +8,7 @@ const Converter = () => {
   const [success, setSuccess] = useState("");
   const [message, setMessage] = useState("");
   const [symbols, setSymbols] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -28,29 +30,34 @@ const Converter = () => {
     const formData = new FormData();
     formData.append("jsonFile", jsonFile);
     formData.append("xlsxFile", excelFile);
-    // Log FormData contents
-
-    console.log(formData);
-
     try {
+      setLoading(true);
       const response = await axios.post(`${BACKEND_URL}`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
 
-      console.log(response.data);
       setSuccess(response.data.message);
       setMessage(response.data.noMatch.message);
       setSymbols(response.data.noMatch.symbols);
+      setLoading(false);
     } catch (error) {
       console.error("Upload error:", error);
     }
   };
 
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <div className="converter">
+        <CalculatorHeading
+          title={"Excel to JSON Swap Converter"}
+          editMode={false}
+          setEditMode={null}
+          visible={false}
+          setShowModal={console.log("Hello")}
+        />
+
         <div className="input-group flex-col">
           <label>Upload valid JSON</label>
           <input type="file" name="jsonFile" accept=".json" onChange={handleFileChange} />
@@ -59,10 +66,30 @@ const Converter = () => {
           <label>Upload valid Excel</label>
           <input type="file" name="excelFile" accept=".xlsx" onChange={handleFileChange} />
         </div>
-        <button onClick={handleUpload}>Upload and Update</button>
+        <button onClick={handleUpload} style={{ minWidth: "190px" }}>
+          {loading ? "Loading..." : "Upload and Update"}
+        </button>
         {success && <p>{success}</p>}
       </div>
-      <div className="flex-col" style={{ alignItems: "center" }}>
+      <div
+        className="flex-col"
+        style={{ alignItems: "center", position: "relative", padding: "2rem 0" }}
+      >
+        {loading && (
+          <div
+            style={{
+              position: "absolute",
+              inset: "0",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              zIndex: "50",
+              background: "oklch(0.21 0.034 264.665)",
+            }}
+          >
+            <h3>Loading ...</h3>
+          </div>
+        )}
         {message && (
           <h3 style={{ textAlign: "left", width: "30%", margin: "0 auto" }}>{message}</h3>
         )}
@@ -74,7 +101,7 @@ const Converter = () => {
           </ul>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
