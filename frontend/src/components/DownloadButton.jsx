8bd98b72a93344e9b5ce9bec980cbee9e32ceb1f
Marko Janjic <marko.m.janjic@gmail.com>
2025-04-01 00:26:08 +0200

    download file fixed

---
+++ frontend/src/components/DownloadButton.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const DownloadButton = ({ jsonFile }) => {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}downloads/updated_${jsonFile.name}`, {
+        responseType: "blob",
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `updated_${jsonFile.name}`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Download failed:", error);
+    }
+  };
+  return (
+    <button style={{ minWidth: "190px" }} onClick={handleDownload}>
+      Download
+    </button>
+  );
+};
+
+export default DownloadButton;
