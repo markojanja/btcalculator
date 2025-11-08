182eef5b8dab5fff43cf2afb7ec0b16f361c4831
Marko Janjic <marko.m.janjic@gmail.com>
2025-11-08 22:58:13 +0100

    add new guide feature added

--- backend/services/userGuides.services.js
+++ backend/services/userGuides.services.js
@@ -1,5 +1,13 @@
 import prisma from "../db/prisma.js";
 
 export const getAllGuides = async () => {
-  return await prisma.guides.findMany({});
+  return await prisma.guides.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+  });
 };
